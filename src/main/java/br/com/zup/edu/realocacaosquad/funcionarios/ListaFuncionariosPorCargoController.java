41d8c2a858ac14507ade1cbfcd88b1b41c645643
Rafael Ponte <rafael.ponte@zup.com.br>
2022-05-22 12:28:14 -0300

    melhora obrigatoriedade do filtro;

--- src/main/java/br/com/zup/edu/realocacaosquad/funcionarios/ListaFuncionariosPorCargoController.java
+++ src/main/java/br/com/zup/edu/realocacaosquad/funcionarios/ListaFuncionariosPorCargoController.java
@@ -2,12 +2,10 @@ package br.com.zup.edu.realocacaosquad.funcionarios;
 
 import br.com.zup.edu.realocacaosquad.client.FuncionariosClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -20,11 +18,7 @@ public class ListaFuncionariosPorCargoController {
     private FuncionariosClient client;
 
     @GetMapping("/api/funcionarios")
-    public ResponseEntity<?> listaPorCargo(@RequestParam CargoDoFuncionario cargo) {
-
-        if (cargo == null) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "filtro de cargo deve ser informado");
-        }
+    public ResponseEntity<?> listaPorCargo(@RequestParam(required = true) CargoDoFuncionario cargo) {
 
         List<FuncionarioFiltradoResponse> funcionarios = client.lista().stream()
                                 .filter(f -> cargo.name().equals(f.getCargo().name()))
